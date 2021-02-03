bec997026db1868057ecd1d2b30e373c659cb8f3
Sebastian Rodriguez <tatanrodriguezg@hotmail.com>
2021-02-03 20:20:12 +0000

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
-  
-  char Mystr[] = "\"Programming is like building a multilingual puzzle";
-     
+  char Mystr[]="\"Programming is like building a multilingual puzzle";
   puts(Mystr);
-     
   return 0;
 }
