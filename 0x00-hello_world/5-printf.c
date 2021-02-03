fa6f5b0ebcd5a022760d4449fdfe034dda2bbbfa
Sebastian Rodriguez <tatanrodriguezg@hotmail.com>
2021-02-03 21:33:32 +0000

    5 fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - function description
+ * Description: used to print
+ * Return: zero is returned
+ */
 int main(void)
 
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
