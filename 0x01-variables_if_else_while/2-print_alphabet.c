a55da0a4bc203ed873442be0dd8cbc77573790c0
Sebastian Rodriguez <tatanrodriguezg@hotmail.com>
2021-02-04 19:28:39 +0000

    2 added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function description
+ * Description: used to print
+ * Return: zero is returned
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
