24f63747866ba3277c217476b80a816f25e665a9
Sebastian Rodriguez <tatanrodriguezg@hotmail.com>
2021-02-04 21:37:06 +0000

    4 is fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-
+/**
+ * main - function description
+ * Description: used to print
+ * Return: zero is returned
+ */
 int main(void)
 {
 char x;
 x = 'a';
 while (x <= 'z')
 {
-if (x == 'e')
-x++;
-else if (x == 'q')
-x++;
-else
-{
-putchar(x);
-x++;
-}
+	if (x == 'e')
+		x++;
+	else if (x == 'q')
+		x++;
+	else
+	{
+		putchar(x);
+		x++;
+	}
 }
 putchar('\n');
 return (0);
