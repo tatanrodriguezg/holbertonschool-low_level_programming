11553f284c5121fe0e7a6baecaa9ec6f9b97decd
Sebastian Rodriguez <tatanrodriguezg@hotmail.com>
2021-02-04 22:25:09 +0000

    5 added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function description
+ * Description: used to print
+ * Return: zero is returned
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	while (x < 9)
+	{
+		printf("%d", x);
+		x++;
+		}
+	printf("%d\n", x);
+	return (0);
+}
