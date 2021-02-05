88fa3b2e525f5c1e63e67b47e1f359543004efc9
Sebastian Rodriguez <tatanrodriguezg@hotmail.com>
2021-02-05 04:43:35 +0000

    fixing 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,19 +6,20 @@
  */
 int main(void)
 {
-	int x, y, z;
+	int x, y, z, w;
 
 	x = 0;
+	w = -38;
 	y = -16;
 	z = -4;
 
-	while (x < 9)
+	while (x <= 9)
 	{
 		putchar('0' + x);
 		x++;
 		putchar('0' + z);
 		putchar('0' + y);
 	}
-	putchar('0' + x);
+	putchar('0' + w);
 	return (0);
 }
